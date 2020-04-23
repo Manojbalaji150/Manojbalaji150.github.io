b0e570ddca19ace3d2a4351edd1a75d505f62bfa
Manojbalaji150 <4mbala2u@gmail.com>
2020-04-23 08:26:42 +0530

    first repo

---
+++ precache-manifest.b37bf11a8dcc58e8f56ff0209d5b6e7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a6ecd2f1fdad044234927921d3af8cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85b95c54f8296b5d7667",
+    "url": "/static/css/main.ffd6e3bb.chunk.css"
+  },
+  {
+    "revision": "333571e5022423809328",
+    "url": "/static/js/2.fa637428.chunk.js"
+  },
+  {
+    "revision": "7f7b7554ad8c1d8206ae278b825bcae0",
+    "url": "/static/js/2.fa637428.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85b95c54f8296b5d7667",
+    "url": "/static/js/main.953bcd19.chunk.js"
+  },
+  {
+    "revision": "56f2a5574c5ae41e392b",
+    "url": "/static/js/runtime-main.a7879073.js"
+  }
+]);
